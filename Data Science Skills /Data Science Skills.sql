c542b2e802e8e08afa3ad57f59b30a1d81571d21
Andres Combaria <cardozoandres337@gmail.com>
2024-03-03 14:59:36 -0500

    second execise solution updated table

--- Data Science Skills /Data Science Skills.sql
+++ Data Science Skills /Data Science Skills.sql
@@ -1,16 +1,21 @@
 create database linkedin;
 use linkedin;
-create table candidates (candidate_id smallint, skill varchar(30));
+create table candidates (
+	candidate_id smallint, 
+	skill varchar(30) not null,
+	primary key (candidate_id),
+	constraint unique (candidate_id,skill)
+);
 insert into candidates values 
-  (123, "Python"),
-    (123, "Tableau"),
+	(123, "Python"),
+	(123, "Tableau"),
     (123, "PostgreSQL"),
     (234, "R"),
     (234, "PowerBI"),
     (234, "SQL Server"),
     (345, "Python"),
     (345, "Tableau");
-SELECT * FROM candidates
+SELECT candidate_id FROM candidates
 WHERE skill IN ("Python","Tableau","PostgreSQL")
 group by candidate_id
 having count(skill)>2;
